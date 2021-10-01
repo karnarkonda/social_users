bdd599be9448bcef58ff449fe3e3b3e8e2758039
Olukayode Akanmu <karnarkonda@gmail.com>
2021-10-02 01:25:39 +0300

    Initial commit

---
+++ social_friends/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class SocialMediaUser(models.Model):
+    user_code = models.CharField(max_length=10, unique=True)
+    first_name = models.CharField(max_length=30, null=True, blank=False)
+    last_name = models.CharField(max_length=30, null=True, blank=False)
+    friends = models.ManyToManyField("SocialMediaUser")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = "Social Media User"
+        verbose_name_plural = "Social Media Users"
